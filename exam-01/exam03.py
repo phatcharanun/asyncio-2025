2fe6cf78e7f264130bb87f2d9c51f43dfcb5b0a4
phatcharanun <153785070+phatcharanun@users.noreply.github.com>
2025-09-17 09:32:32 +0700

    finishExam03

--- exam-01/exam03.py
+++ exam-01/exam03.py
@@ -10,7 +10,8 @@ async def compute(x, y):
     return x + y
 
 async def main():
-    result = compute(2, 3)
+    result = await compute(2, 3)
+    #result = compute(2, 3)
     print("Result:", result)
 
 asyncio.run(main())
